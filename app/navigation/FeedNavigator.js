3a5a4058428d97873d31069d629dac3162886658
marroncode <marandege@gmail.com>
2025-01-14 13:51:59 +0200

    center the listing header title

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Platform } from 'react-native'
+import { Platform } from "react-native";
 
 import ListingsScreen from "../screens/ListingsScreen";
 import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 
+const Stack = createNativeStackNavigator();
 
-const Stack= createNativeStackNavigator();
-
-const FeedNavigator=()=>(
-    <Stack.Navigator screenOptions={
-        Platform.select({//set platform specific definitions
-            ios:{
-                presentation:"modal",
-                headerShown:false
-                
-            }
-        })
-        }>
-        <Stack.Screen name="Listings" component={ListingsScreen}/>
-        <Stack.Screen name="ListingDetail" component={ListingDetailsScreen}/>
-    </Stack.Navigator>
+const FeedNavigator = () => (
+  <Stack.Navigator
+    screenOptions={Platform.select({
+      //set platform specific definitions
+      ios: {
+        presentation: "modal",
+        headerShown: false,
+      },
+    })}
+  >
+    <Stack.Screen
+      name="Listings"
+      component={ListingsScreen}
+      options={{
+        // headerShown: false,
+        headerTitleAlign: "center",
+      }}
+    />
+    <Stack.Screen name="ListingDetail" component={ListingDetailsScreen} />
+  </Stack.Navigator>
 );
 
 export default FeedNavigator;
